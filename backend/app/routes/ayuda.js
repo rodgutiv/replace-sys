5f7e1949a737117ebf2885ba22e3a27ccacfba35
rvazquez <gutierrez.v.rod@gmail.com>
2019-03-29 14:59:46 -0600

    mail

--- backend/app/routes/ayuda.js
+++ backend/app/routes/ayuda.js
@@ -5,15 +5,34 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
 
   var data = req.body;
-  console.log(data)
+  console.log(data);
   var name = data[0].value;
   var from = data[1].value;
-  var message = data[3].value;
-
+  var message = data[2].value;
+  console.log(name);
+  console.log(from);
+  console.log(message);
   var nodemailer = require('nodemailer');
   // create reusable transporter object using the default SMTP transport
-  var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+  console.log('cargo nodemailer');
+  var data = 'smtps://' + from + ':pass'
+  //var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+  var transporter = nodemailer.createTransport({
+      protocol: "SMTP",
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      secureOptions: { "rejectUnauthorized": false },
+      tls: {
+          rejectUnauthorized: false,
+      },
+      auth: {
+          user: from,
+          pass: 'confundidoro'
+      }
+  })
   // setup e-mail data with unicode symbols
+    console.log('cargo transporte');
   var mailOptions = {
       from: '"'+name+'" <'+from+'>', // sender address
       to: 'gutierrez.v.rod@gmail.com', // list of receivers
@@ -21,6 +40,7 @@ router.post('/', function(req, res, next) {
       text: message, // plaintext body
       html: '<b>Enviado desde reface.com</b>' // html body
   };
+    console.log('cargo opciones de mail');
   // send mail with defined transport object
   transporter.sendMail(mailOptions, function(error, info){
       if(error){
